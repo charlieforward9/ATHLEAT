a1a902ea6901c9472a9f3c21eb51905d84766884
charlieforward9 <62311337+charlieforward9@users.noreply.github.com>
2023-10-25 17:24:03 -0400

    feat: init user input forms

---
+++ src/app/forms/mood/page.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import React, { ChangeEvent, FormEvent, useState } from "react";
+
+enum Mood {
+  "Very Bad" = 1,
+  "Bad",
+  "Neutral",
+  "Good",
+  "Very Good",
+}
+
+function NutritionForm() {
+  // State to keep track of the input values
+  const [mood, setMood] = useState(5);
+
+  // Handler for input value changes
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setMood(Number(e.target.value));
+  };
+
+  // Handle form submission
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    // Clear the form
+    setMood(0);
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+      <div className="w-full max-w-md bg-white rounded-lg shadow-md p-6">
+        <h1 className="text-2xl font-semibold mb-4 text-center">Mood Form</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2">
+              How are you feeling right now?:
+            </label>
+            <div className="flex flex-row space-x-5">
+              <input
+                type="range"
+                min="1"
+                max="5"
+                name="mood"
+                value={mood}
+                onChange={handleChange}
+                required
+                className="shadow  w-2/3 appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              />
+              <p className="text-xl min-w-sm w-1/3 text-center">{Mood[mood]}</p>
+            </div>
+          </div>
+
+          <div className="flex items-center justify-between">
+            <button
+              type="submit"
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            >
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default NutritionForm;
---
+++ src/app/forms/nutrition/page.tsx
@@ -0,0 +1,135 @@
+"use client";
+
+import React, { ChangeEvent, FormEvent, useState } from "react";
+
+function NutritionForm() {
+  // State to keep track of the input values
+  const [formData, setFormData] = useState({
+    date: "",
+    time: "",
+    food: "",
+    quantity: 1,
+  });
+
+  // Handler for input value changes
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  // Handle form submission
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    // Clear the form
+    setFormData({ date: "", time: "", food: "", quantity: 1 });
+  };
+
+  const setDateTimeToNow = () => {
+    const now = new Date();
+    const date = now.toISOString().split("T")[0];
+    const time = now.toTimeString().split(" ")[0];
+    setFormData((prevState) => ({
+      ...prevState,
+      date,
+      time,
+    }));
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+      <div className="flex flex-col w-full max-w-md bg-white rounded-lg shadow-md p-6 items-center">
+        <h1 className="text-2xl font-semibold mb-4 text-center">
+          Nutrition Form
+        </h1>
+        <div className="flex items-center justify-between">
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            onClick={setDateTimeToNow}
+          >
+            Now
+          </button>
+        </div>
+
+        <form
+          className="flex flex-col items-center w-full"
+          onSubmit={handleSubmit}
+        >
+          <div className="mb-4 w-full">
+            <label className="block text-gray-700 text-sm font-bold mb-2">
+              Date:
+            </label>
+            <input
+              type="date"
+              name="date"
+              value={formData.date}
+              onChange={handleChange}
+              required
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            />
+          </div>
+          <div className="mb-4 w-full">
+            <label className="block text-gray-700 text-sm font-bold mb-2">
+              Time:
+            </label>
+            <input
+              type="time"
+              name="time"
+              value={formData.time}
+              onChange={handleChange}
+              required
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              step="1"
+            />
+          </div>
+          <div className="mb-6 w-full">
+            <label className="block text-gray-700 text-sm font-bold mb-2">
+              Food:
+            </label>
+            <input
+              type="text"
+              name="food"
+              value={formData.food}
+              onChange={handleChange}
+              required
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            />
+          </div>
+          <div className="mb-6 w-full">
+            <label className="block text-gray-700 text-sm font-bold mb-2">
+              Quantity:
+            </label>
+            <div className="flex flex-row">
+              <input
+                type="range"
+                min="1"
+                max="10"
+                name="quantity"
+                value={formData.quantity}
+                onChange={handleChange}
+                required
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              />
+              <p className="text-xl min-w-sm w-1/3 text-center">
+                {formData.quantity}
+              </p>
+            </div>
+          </div>
+          <div className="flex items-center">
+            <button
+              type="submit"
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            >
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default NutritionForm;
